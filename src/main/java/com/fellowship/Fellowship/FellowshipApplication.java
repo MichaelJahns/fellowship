d0fba68f8d91b9b9880637283ee838c0d19ba1a3
MichaelJahns <jahnsmic001@gmail.com>
2019-05-07 10:19:11 -0700

    Init

---
+++ src/main/java/com/fellowship/Fellowship/FellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.fellowship.Fellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FellowshipApplication.class, args);
+	}
+
+}
